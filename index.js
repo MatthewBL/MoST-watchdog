647f57763c50bd2cc60aae7d0ab15cb4a3fee2bd
Matthew BL <xamtehwt@gmail.com>
2026-09-02 13:25:31 +0200

    Accommodate code to previous version of node

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const { execSync } = require("node:child_process");
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
 const STORAGE_FILE = path.join(__dirname, "last-squeue.json");
 const ENV_FILE = path.join(__dirname, ".env");
